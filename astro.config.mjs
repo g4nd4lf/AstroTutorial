65739c0451037da34ca8846f24d9d6bfc88248a3
g4nd4lf <rafaelromerovicente@gmail.com>
2024-02-08 21:28:32 +0100

    finished tutorial "Build a Blog"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://funny-lamington-b4f64f.netlify.app",
+  integrations: [preact()]
+});
